83375b208bb4c9ce42e02c81d05ef22e0c9715d4
Mehmet Ali SAHINOGULLARI <mehmetalisahinogullari@gmail.com>
2017-07-11 22:59:11 +0300

    Update Async Patterns.js

--- Task4/Async Patterns.js
+++ Task4/Async Patterns.js
@@ -2,3 +2,19 @@
 // Callback Functions
 // Generators
 // Promises
+
+
+
+/* ------------------------------ Bad Code  ------------------------------------*/
+
+/*---------- CALLBACKS ----------*/
+
+setTimeout(function () {
+    console.log("First");
+    setTimeout(function () {
+        console.log("Two");
+        setTimeout(function () {
+            console.log("Three");
+        }, 1000);
+    }, 1000);
+}, 1000);
