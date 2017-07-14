b15c0cbdbfd4dfa9f9e244abbb132efb93f77c35
Mehmet Ali SAHINOGULLARI <mehmetalisahinogullari@gmail.com>
2017-07-14 17:40:47 +0300

    Update Profile.js

--- Task3/Profile.js
+++ Task3/Profile.js
@@ -7,40 +7,42 @@ function urlObject() {
 }
 
 
-var emarTest = new urlObject();
-emarTest.domainUrl = "www.emarTest.domainUrl.com";
-emarTest.securityUrl = "www.emarTest.security.com";
-emarTest.uygulamaUrl = "www.emarTest.uygulama.com";
+var company1Test = new urlObject();
+company1Test.domainUrl = "www.company1Test.domainUrl.com";
+company1Test.securityUrl = "www.company1Test.security.com";
+company1Test.uygulamaUrl = "www.company1Test.uygulama.com";
 
-var emarCanli = new urlObject();
-emarCanli.domainUrl = "www.emarCanli.domainUrl.com";
-emarCanli.securityUrl = "www.emarCanli.security.com";
-emarCanli.uygulamaUrl = "www.emarCanli.uygulama.com";
+var company1Prod = new urlObject();
+company1Prod.domainUrl = "www.company1Prod.domainUrl.com";
+company1Prod.securityUrl = "www.company1Prod.security.com";
+company1Prod.uygulamaUrl = "www.company1Prod.uygulama.com";
 
-var daikinTest = new urlObject();
-daikinTest.domainUrl = "www.daikinTest.domainUrl.com";
-daikinTest.securityUrl = "www.daikinTest.security.com";
-daikinTest.uygulamaUrl = "www.daikinTest.uygulama.com";
+var company2Test = new urlObject();
+company2Test.domainUrl = "www.company2Test.domainUrl.com";
+company2Test.securityUrl = "www.company2Test.security.com";
+company2Test.uygulamaUrl = "www.company2Test.uygulama.com";
 
-var  daikinProd = new urlObject();
-daikinProd.domainUrl = "www.daikinProd.domainUrl.com";
-daikinProd.securityUrl = "www.daikinProd.security.com";
-daikinProd.uygulamaUrl = "www.daikinProd.uygulama.com";
+var  company2Prod = new urlObject();
+company2Prod.domainUrl = "www.company2Prod.domainUrl.com";
+company2Prod.securityUrl = "www.company2Prod.security.com";
+company2Prod.uygulamaUrl = "www.company2Prod.uygulama.com";
 
-var baymakTest = new urlObject();
-baymakTest.domainUrl = "www.baymakTest.domainUrl.com";
-baymakTest.securityUrl = "www.baymakTest.security.com";
-baymakTest.uygulamaUrl = "www.baymakTest.uygulama.com";
+var company3Test = new urlObject();
+company3Test.domainUrl = "www.company3Test.domainUrl.com";
+company3Test.securityUrl = "www.company3Test.security.com";
+company3Test.uygulamaUrl = "www.company3Test.uygulama.com";
 
-var baymakProd = new urlObject();
-baymakProd.domainUrl = "www.baymakProd.domainUrl.com";
-baymakProd.securityUrl = "www.baymakProd.security.com";
-baymakProd.uygulamaUrl = "www.baymakProd.uygulama.com";
+var company3Prod = new urlObject();
+company3Prod.domainUrl = "www.company3Prod.domainUrl.com";
+company3Prod.securityUrl = "www.company3Prod.security.com";
+company3Prod.uygulamaUrl = "www.company3Prod.uygulama.com";
 
 var profil = {}
 
-profil.__proto__ = emarCanli;
+profil.__proto__ = company1Prod;
 
 var uygulamaUrl = profil.uygulamaUrl;
 var securityUrl = profil.securityUrl;
 var domainUrl = profil.domainUrl;
+
+
