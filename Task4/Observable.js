0e787eb333a559eee6e03eb133943341d43ecb6d
Mehmet Ali SAHINOGULLARI <mehmetalisahinogullari@gmail.com>
2017-07-15 21:46:31 +0300

    Update Observable.js

--- Task4/Observable.js
+++ Task4/Observable.js
@@ -1,1 +1,21 @@
 
+
+var Book = function (name, price) {
+    this.name = function (val) {
+
+    };
+
+    this.price = function () {
+
+    };
+
+    this.onPriceChanging = function (callback) {
+
+    };
+
+    this.onPriceChanged = function () {
+
+    };
+};
+
+
